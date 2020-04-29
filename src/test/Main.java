c8a37e73fb6928e6a12e0cbe6d30aff0855f824a
Illya <illya-komar@ukr.net>
2020-04-29 19:09:54 +0300

    Update exercise

--- src/test/Main.java
+++ src/test/Main.java
@@ -13,9 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
-		{1, 2, 3, 4},
-		{5, 6, 7, 8},
-		{9, 10, 11, 12}
+		{11, 22, 33, 44},
+		{55, 66, 77, 88},
+		{99, 10, 11, 12}
        };
 		
         int[][] transposed = Exercise.transpose(matrix);
